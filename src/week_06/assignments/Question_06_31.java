3966521b6dd9fe4785bc8379a39de787ab746a03
yusuf <yusufemiruyanik.gmail.com>
2023-09-28 11:35:54 +0300

    chapter6 was completed and question 31 was  solved again

--- src/week_06/assignments/Question_06_31.java
+++ src/week_06/assignments/Question_06_31.java
@@ -14,55 +14,65 @@ public class Question_06_31 {
     }
 
     public static boolean isValid(long creditCardNumber) {
-        boolean result = false;
-        int total1 = sumOfDoubleEvenPlace(creditCardNumber) ;
-        int total2 = sumOfOddPlace(creditCardNumber) ;
-        if ((total1+total2) % 10 == 0) {
-            result = true;
+        boolean result = true;
+        int sumOfDoubleEvenPlace = sumOfDoubleEvenPlace(creditCardNumber);
+        int sumOfOddPlace = sumOfOddPlace(creditCardNumber);
+        if ((sumOfOddPlace + sumOfDoubleEvenPlace) % 10 != 0) {
+            return false;
+        }
+        if (!prefixMatched(creditCardNumber)) {
+            return false;
+        }
+        if (!isLengthOfCreditCard(creditCardNumber)) {
+            return false;
         }
         return result;
+
     }
 
-    public static int sumOfDoubleEvenPlace(long number) {
-        String creditCard = number + "";
+    public static int sumOfDoubleEvenPlace(long creditCard) {
+        String strOfCreditCaard = creditCard + "";
         int total = 0;
-
-        for (int i = creditCard.length() - 1; i >= 0; i--) {
-
-            if (i % 2 == 0) {
-                int numb = Integer.parseInt(creditCard.charAt(i) + "");
-                numb*=2;
-                total+=getDigit(numb);
-            }
-
-
-
-
+        for (int i = 0; i <= strOfCreditCaard.length() - 1; i += 2) {
+            int digit = strOfCreditCaard.charAt(i) - '0';
+            digit *= 2;
+            total += getDigit(digit);
         }
         return total;
     }
 
-    public static int getDigit(int numb) {
-        int result = 0;
-        if (numb > 9) {
-            int digit1 = numb % 10;
-            int digit2 = numb / 10;
-            result = digit1 + digit2;
-        } else {
-            result = numb;
+    public static int getDigit(int digit) {
+        if (digit > 9) {
+            int numb1 = digit / 10;
+            int numb2 = digit % 10;
+            return numb1 + numb2;
         }
-        return result;
+        return digit;
     }
 
-    public static int sumOfOddPlace(long number) {
-        String creditCard = number + "";
+    public static int sumOfOddPlace(long creditCard) {
         int total = 0;
-        for (int i = creditCard.length() - 1; i >= 0; i--) {
-            if (i % 2 == 1) {
-                int numb = Integer.parseInt(creditCard.charAt(i) + "");
-                total += numb;
-            }
+        String strOfCreditCard = creditCard + "";
+        for (int i = 1; i <= strOfCreditCard.length() - 1; i += 2) {
+            total += strOfCreditCard.charAt(i) - '0';
         }
         return total;
+    }
+
+    public static boolean prefixMatched(long creditCard) {
+        String strOfCreditCaard = creditCard + "";
+        if (strOfCreditCaard.startsWith("4") || strOfCreditCaard.startsWith("37")
+                || strOfCreditCaard.startsWith("5") || strOfCreditCaard.startsWith("6")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isLengthOfCreditCard(long creditCard) {
+        String strOfCreditCaard = creditCard + "";
+        if (strOfCreditCaard.length() >= 13 && strOfCreditCaard.length() <= 16) {
+            return true;
+        }
+        return false;
     }
 }
